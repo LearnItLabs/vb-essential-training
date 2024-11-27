7f62fd5b735fb74233d61aba471cef8d278d96b5
Walt Ritscher <waltritscher@outlook.com>
2024-11-26 18:33:52 -0800

    Create ToDo list example

--- code/05-Flow/SimpleDesktop/FinishedWindow.xaml.vb
+++ code/05-Flow/SimpleDesktop/FinishedWindow.xaml.vb
@@ -2,13 +2,12 @@
 
 Public Class FinishedWindow
 
+	Private Sub AddToDoButton_Click(sender As Object, e As RoutedEventArgs) Handles AddToDoButton.Click
+		ToDoList.Items.Add(TextBox1.Text)
 
-	Private Sub outputTheDateButton_Click(sender As Object, e As RoutedEventArgs) Handles outputTheDayButton.Click
-		textBlock1.Text = DateTime.Now.DayOfWeek.ToString
-		Me.Background = New SolidColorBrush(Colors.LightBlue)
 	End Sub
 
-	Private Sub textBox1_TextChanged(sender As Object, e As TextChangedEventArgs) Handles textBox1.TextChanged
-		textBlock1.Text = textBox1.Text & "-"
+	Private Sub toDoList_SelectionChanged(sender As Object, e As SelectionChangedEventArgs) Handles toDoList.SelectionChanged
+		Me.TextBlock1.Text = ToDoList.SelectedItem.ToString
 	End Sub
 End Class
