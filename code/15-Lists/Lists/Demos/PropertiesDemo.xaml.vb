64d49dfe6c39edfe1559c42d314d2b4cb9f3a9bc
Walt Ritscher <waltritscher@outlook.com>
2024-12-16 23:54:59 -0800

    remove some examples

--- code/15-Lists/Lists/Demos/PropertiesDemo.xaml.vb
+++ code/15-Lists/Lists/Demos/PropertiesDemo.xaml.vb
@@ -1,5 +1,10 @@
 ﻿Public Class PropertiesDemo
+	Private Sub ReverseButton_Click(sender As Object, e As RoutedEventArgs) Handles ReverseButton.Click
+		Dim brandColors = New BrandColors()
+		Dim colorNames = brandColors.ColorNames
+		' no demo for this section.
 
+	End Sub
 
 
 
