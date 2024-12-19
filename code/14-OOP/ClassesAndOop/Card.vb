a1ceaf4095e201f88793a4d70032e946dbae67ff
Walt Ritscher <waltritscher@outlook.com>
2024-12-18 17:22:57 -0800

    add oop examples

--- code/14-OOP/ClassesAndOop/Card.vb
+++ code/14-OOP/ClassesAndOop/Card.vb
@@ -1,84 +1,124 @@
 ﻿' Base class Card
 Imports System.Runtime.Serialization
+Namespace BigStarCards
+  ' classes defined for a fantasy trading card game
+  Public Class Card
 
-' classes defined for a fantasy trading card game
-Public Class Card
+#Region "Constructors"
+    ' In Visual Basic, when you define a Sub New procedure 
+    ' it acts as the constructor for the class. If you define a Sub New without parameters,
+    ' it replaces the default constructor provide by .NET
 
-  Private _name As String
-  Public Property Name() As String
-    Get
-      Return _name
-    End Get
-    Set(ByVal value As String)
-      _name = value
-    End Set
-  End Property
-  ' Auto-implemented properties
-  Public Property Description As String
-  Public Property ListPrice As Decimal
-  Public Property AskingPrice As Decimal
+    Public Sub New()
+      ' The primary purpose of a constructor
+      ' is to initialize the object by setting up its initial state.
+      ' This typically involves assigning values to fields,
+      ' initializing properties, or performing
+      ' any setup required for the object to function properly.
 
+    End Sub
+    Public Sub New(name As String, description As String,
+                   originalListPrice As Decimal, askingPrice As Decimal)
+      ' an additional constructor that takes four parameters
+      Me.Name = name
+      Me.Description = description
+      Me.OriginalListPrice = originalListPrice
+      Me.AskingPrice = askingPrice
+    End Sub
 
-  Public ReadOnly Property Discount As Decimal
-    Get
-      Return AskingPrice * 0.45D
-    End Get
-  End Property
-
-
-  Public ReadOnly Property SalePrice As Decimal
-    Get
-      Return AskingPrice - Discount
-    End Get
-  End Property
-
-
-  Public ReadOnly Property TypeName As String
-    Get
-      Return Me.GetType().FullName
-    End Get
-  End Property
-End Class
-
-
-Public Class Creature
-  Inherits Card
-
-  Public Property EyeCount As Integer
-  Public Property Spikes As Boolean
-  Public Property Antenna As Boolean
-End Class
-
-
-Public Class Monster
-  Inherits Creature
-End Class
+    Public Sub New(name As String, description As String)
+      ' an additional constructor that takes two parameters
+      Me.Name = name
+      Me.Description = description
+      Me.OriginalListPrice = OriginalListPrice
+      Me.AskingPrice = AskingPrice
+    End Sub
+#End Region
+    Private _name As String
+    Public Property Name() As String
+      Get
+        Return _name
+      End Get
+      Set(ByVal value As String)
+        _name = value
+      End Set
+    End Property
+    ' Auto-implemented properties
+    Public Property Description As String
+    Public Property OriginalListPrice As Decimal
+    Public Property AskingPrice As Decimal
 
 
-Public Class Robot
-  Inherits Card
-  Implements IComparable(Of Robot)
+    Public ReadOnly Property Discount As Decimal
+      Get
+        Return AskingPrice * 0.45D
+      End Get
+    End Property
 
 
-  Public Property BatteryLevel As Decimal
-
-  Public Function CompareTo(other As Robot) As Integer Implements IComparable(Of Robot).CompareTo
-    If other Is Nothing Then
-      Return 1 ' Treat current instance as greater if other is null
-    End If
-
-    ' Compare BatteryLevel
-    Return Me.BatteryLevel.CompareTo(other.BatteryLevel)
-  End Function
-End Class
+    Public ReadOnly Property SalePrice As Decimal
+      Get
+        Return AskingPrice - Discount
+      End Get
+    End Property
 
 
-Public Class Cyborg
-  Inherits Robot
-End Class
+    Public ReadOnly Property TypeName As String
+      Get
+        Return Me.GetType().FullName
+      End Get
+    End Property
+  End Class
 
 
-Public Class Nanobot
-  Inherits Robot
-End Class
+  Public Class Creature
+    Inherits Card
+
+    Public Property EyeCount As Integer
+    Public Property Spikes As Boolean
+    Public Property Antenna As Boolean
+  End Class
+
+
+  Public Class Monster
+    Inherits Creature
+  End Class
+
+
+  Public Class Robot
+    Inherits Card
+    Implements IComparable(Of Robot)
+
+    Public Property BatteryLevel As Decimal
+
+    Public Function CompareTo(other As Robot) As Integer Implements IComparable(Of Robot).CompareTo
+      If other Is Nothing Then
+        Return 1 ' Treat current instance as greater if other is null
+      End If
+
+      ' Compare BatteryLevel
+      Return Me.BatteryLevel.CompareTo(other.BatteryLevel)
+    End Function
+    ' Overload the < operator
+    Public Shared Operator <(r1 As Robot, r2 As Robot) As Boolean
+      Return r1.CompareTo(r2) < 0
+    End Operator
+
+    ' Overload the > operator
+    Public Shared Operator >(r1 As Robot, r2 As Robot) As Boolean
+      Return r1.CompareTo(r2) > 0
+    End Operator
+  End Class
+
+
+  Public Class Cyborg
+    Inherits Robot
+  End Class
+
+
+  Public Class Nanobot
+    Inherits Robot
+  End Class
+End Namespace
+
 
---
+++ code/14-OOP/ClassesAndOop/Demos/CardDemo.xaml.vb
@@ -0,0 +1,118 @@
+﻿Imports ClassesAndOop.BigStarCards
+
+Public Class CardDemo
+
+	Private Sub SetButton_Click(sender As Object, e As RoutedEventArgs) Handles SetButton.Click
+		Dim card = New BigStarCards.Card
+
+		card = SetCardProperties(card)
+
+		' read properties
+
+		Dim salePrice = card.SalePrice ' a read only property
+
+		Dim cardName = card.Name
+		ShowHeader("Card Name")
+		ShowIt(cardName)
+
+		ShowHeader("Sale Price")
+		ShowIt(salePrice)
+
+	End Sub
+
+	Private Shared Function SetCardProperties(card As BigStarCards.Card) As BigStarCards.Card
+		' set property
+		card.Name = "Traxtor"
+		card.Description = "Traxtor: A relentless warrior who crushes foes with unyielding strength and ferocity."
+		card.OriginalListPrice = 5.95D
+		card.AskingPrice = 75D
+		Return card
+	End Function
+
+	Private Sub ConstructorButton_Click(sender As Object, e As RoutedEventArgs) Handles ConstructorButton.Click
+		Dim card1 = New BigStarCards.Card ' use parameterless constructor
+		Dim emberDesc As String = "A fiery spirit that ignites the battlefield with bursts of flame."
+
+		Dim card2 = New BigStarCards.Card("Emberling", emberDesc)
+	End Sub
+
+	Private Sub InheritButton_Click(sender As Object, e As RoutedEventArgs) Handles InheritButton.Click
+		Dim card As New BigStarCards.Card
+		Dim bot As New BigStarCards.Nanobot
+		Dim monster = New BigStarCards.Monster
+
+		' work with base class properties
+
+		monster.AskingPrice = 55D
+		bot.AskingPrice = 12.44D
+
+		' work with derived class properties
+		monster.Antenna = True
+		bot.BatteryLevel = 0.5
+
+
+	End Sub
+
+	Private Sub InterfaceButton_Click(sender As Object, e As RoutedEventArgs) Handles InterfaceButton.Click
+
+		Dim bot1 As New Nanobot
+		Dim bot2 As New Nanobot
+		Dim monster1 = New BigStarCards.Monster
+		Dim monster2 = New BigStarCards.Monster
+
+		Dim sortedMonsters As New SortedSet(Of Monster)
+
+		' cannot use SortedSet unless the type
+		' implements IComparable (Of T)
+		'sortedMonsters.Add(monster1)
+		'sortedMonsters.Add(monster2)
+
+		Dim sortedBots As New SortedSet(Of BigStarCards.Robot)
+
+		sortedBots.Add(bot1)
+		sortedBots.Add(bot2)
+
+
+		'If bot1 > bot2 Then
+
+		'End If
+
+	End Sub
+
+#Region "Show methods"
+
+
+	Sub ShowIt(message As String)
+		Dim bulletText As New Run("» ")
+		MessageOut.Inlines.Add(bulletText)
+		bulletText.Foreground = Brushes.SteelBlue
+		Dim normalText As New Run(message + vbCrLf)
+		MessageOut.Inlines.Add(message + vbCrLf)
+	End Sub
+	Sub ShowBlankLine()
+		Dim lineText As New Run("" + vbCrLf)
+		MessageOut.Inlines.Add(lineText)
+	End Sub
+	Sub ShowHeader(message As String)
+		Dim boldText As New Run(message + vbCrLf)
+		boldText.FontWeight = FontWeights.Bold
+		MessageOut.Inlines.Add(boldText)
+
+	End Sub
+	Sub Clear()
+		MessageOut.Text = ""
+	End Sub
+
+	Sub ShowLine()
+		Dim lineText As New Run(New String("-"c, 68) + vbCrLf)
+		lineText.Foreground = Brushes.LightBlue
+		MessageOut.Inlines.Add(lineText)
+	End Sub
+
+
+
+
+
+
+#End Region
+End Class
