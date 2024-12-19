33e1d3ffe30135e37b9d447881260f5de1533304
walt-instructor <staffauthor@outlook.com>
2024-12-18 17:21:15 -0800

    Add try catch example

--- code/17-Error/Errors/Demos/AddTryCatch.xaml.vb
+++ code/17-Error/Errors/Demos/AddTryCatch.xaml.vb
@@ -43,25 +43,7 @@ Public Class AddTryCatch
 	End Sub
 
 	Private Sub ButtonD_Click(sender As Object, e As RoutedEventArgs) Handles ButtonD.Click
-		Try
-			_counter += 1
-			Dim result As Integer = GetDataFromList(20)
-			FileATextBox.Text = _counter
-			FileBTextBox.Text = ErrorMaker.OpenFile("c:\nofile.txt")
-			result = ErrorMaker.WorkWithNumbers(7)
 
-		Catch ex As System.IO.FileNotFoundException
-			MessageBox.Show($"Cannot access the file  { ex.FileName}")
-		Catch ex As OverflowException
-			MessageBox.Show($"This looks like a division error")
-		Catch ex As Exception
-
-			' this block is for unexpected exceptions
-			' best log the exception
-			MessageBox.Show($"Something went wrong { ex.Message}")
-			My.
-			'My.Log.WriteException(ex, TraceEventType.Critical, "Add a custom log message")
-		End Try
 
 	End Sub
 End Class
